d37933dedd98b56de248eec547926bfaba518905
shinheewon <qjqtktnf@naver.com>
2024-08-07 17:44:18 +0900

    어렵다

---
+++ 0807_stack/종이 붙이기/paste_paper.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open("sample_input.txt", "r")
+
+test_case = int(input())
+for tc in range(1, test_case + 1):
+    N = int(input()) // 10   # 가로 길이
+
+
+# n=1
+경우의 수는 1
+
+# n=2
+경우의 수는 3
+
+# n=3
+3칸
+# n=4
+# n=5
+# n=6
+# n=7
+# n=8
+# n=9
+# n=10
