4423e20e7d99b0c158c53c97b389bbff448b6b55
shinheewon <shinheewon0107@gmail.com>
2024-08-08 21:18:10 +0900

    역순으로 길찾기

--- 0807_stack/길찾기/find_road.py
+++ 0807_stack/길찾기/find_road.py
@@ -6,35 +6,36 @@ sys.stdin = open("input.txt", "r")
 
 def dfs_m(s, adj_list):
     # 방문체크
-    visited = [[0] * 100]  # 노드는 100개다.
+    # visited = [[0] * 100]  # 노드는 100개다.
+    visited = [0 for _ in range(100)]  # 노드는 100개다.
 
     # 스택설정
     stack = []
 
     # 시작 정점은 방문 처리
     visited[s] = 1
-
+    stack.append(s)
     # 현재 방문하고 있는 정점
     v = s
 
     while True:
-        for i in range(2):
-            if adj_list[v][i] and visited[adj_list[v][i]] == 0: # 노드가 있고, 방문기록이 없다.
-                v = adj_list[v][i]  # 그 방으로 정점 이동
-                visited[v] = 1
-                stack.append(v)
-                print(stack)        # 스택을 쌓아준다.
-                if v == 99:
-                    print(f"#{shop} 1")
-                    break
-            else:   # 방이 비어있으면
-                v = stack.pop() # 스택 줄어들고 전 방으로 간다.
-        else:   # break가 된적이 없으면.
-            print(f"#{shop} 0")
-            break
+        if v == 99:
+            return 1
 
+        if adj_list[v] and visited[adj_list[v][-1]] == 0:    # 노드가 있고, 방문기록이 없다.
+            v = adj_list[v][-1]  # 그 방으로 정점 이동
+            visited[v] = 1
+            stack.append(v)
+        else:   # 방이 비어있으면
+            if stack:   # 돌아갈 스택이 있다면
+                v = stack.pop()     # 스택 줄어들고 전 노드로 간다.
+                if adj_list[v]:     # 전 노드에 간선이 있으면
+                    adj_list[v].pop()   # 우리는 뒤에서부터 순회했고 중복이 되니 팝해준다.
+            else:   # 돌아갈 스택이 없으면 종료. 길이 없다.
+                break
+    return 0
 
-test_case = 1
+test_case = 10
 for tc in range(1, test_case +1):
     shop, V = map(int,input().split())      # s = 날리기, 간선쌍 개수
     arr = list(map(int, input().split()))
@@ -43,5 +44,4 @@ for tc in range(1, test_case +1):
         v1, v2 = arr[i], arr[i+1]   # 시작 노드와 도착 노드 할당.
         adj_list[v1].append(v2)     # 각 노드의 도착 노드들을(최대 2개) 할당
 
-
-dfs_m(0, adj_list)   # 시작 노드, 간선 쌍 개수, 인접 리스트
+    print(f"#{tc} {dfs_m(0, adj_list)}")   # 시작 노드, 간선 쌍 개수, 인접 리스트
