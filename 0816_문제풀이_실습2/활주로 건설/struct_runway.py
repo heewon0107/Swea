4d6f1b455d69c5da5d4239dc5aaea3a33605ad26
shinheewon0107 <shinheewon0107@gmail.com>
2024-08-18 20:30:01 +0900

    construct_runway

--- 0816_문제풀이_실습2/활주로 건설/struct_runway.py
+++ 0816_문제풀이_실습2/활주로 건설/struct_runway.py
@@ -4,6 +4,7 @@ sys.stdin = open("sample_input.txt", "r")
 
 
 def check_i(idx):  # 인덱스 범위 안에서 탐색한다.   0~N-1
+    construct = [0] * N
     for k in range(1, N):
         # 왼쪽이 클 경우 = 오른쪽에 경사로를 놓아야함
         if arr[idx][k - 1] > arr[idx][k]:
@@ -12,6 +13,9 @@ def check_i(idx):  # 인덱스 범위 안에서 탐색한다.   0~N-1
                     return False
                 if arr[idx][k - 1] - 1 != arr[idx][k + stack]:  # 경사로 길이 만큼 같아야 함.
                     return False
+                if construct[k+stack]:  # 건설한 경사로가 있다면 건설 불가
+                    return False
+                construct[k + stack] = 1    # 경사로 건설
         # 오른쪽이 클 경우 = 왼쪽에 경사로를 놓아야 함.
         elif arr[idx][k - 1] < arr[idx][k]:
             for stack in range(X):
@@ -19,7 +23,8 @@ def check_i(idx):  # 인덱스 범위 안에서 탐색한다.   0~N-1
                     return False
                 if arr[idx][k - 1 - stack] != arr[idx][k] - 1:
                     return False
-
+                if construct[k - 1 - stack]:
+                    return False
     return True
 
 
@@ -33,8 +38,9 @@ def check_j(jdx):  # 인덱스 범위 안에서 탐색한다.  위아래 탐색
                     return False
                 if arr[k - 1][jdx] - 1 != arr[k + stack][jdx]:  # 경사로 길이 만큼 같아야 함.
                     return False
-                if not construct[k + stack]:
+                if construct[k + stack]:
                     return False
+
                 construct[k + stack] = 1
         # 아래가 더 클 경우 = 위에 경사로를 배치해야함
         elif arr[k][jdx] > arr[k - 1][jdx]:
@@ -43,7 +49,9 @@ def check_j(jdx):  # 인덱스 범위 안에서 탐색한다.  위아래 탐색
                     return False
                 if arr[k][jdx] - 1 != arr[k - 1 - stack][jdx]:  # 경사로 길이 만큼 같아야 함.
                     return False
-
+                if construct[k - 1 - stack]:
+                    return False
+                construct[k - 1 - stack] = 1
     return True
 
 T = int(input())
@@ -52,9 +60,9 @@ for tc in range(1, T + 1):
     arr = [list(map(int, input().split())) for _ in range(N)]
     count = 0
     for x in range(N):
-        if check_i(x):
+        if check_i(x):  # 가로방향 경사로 건설
             count += 1
-        if check_j(x):
+        if check_j(x):  # 세로방향 경사로 건설
             count += 1
 
     print(f"#{tc} {count}")
