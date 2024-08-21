d35bf189a954be77c12c6d6709bae2364227a69f
shinheewon0107 <shinheewon0107@gmail.com>
2024-08-21 21:24:25 +0900

    Fish_shaped_bun

--- 2주차 추가문제/진기의 최고급 붕어빵/Fish_shaped_bun.py
+++ 2주차 추가문제/진기의 최고급 붕어빵/Fish_shaped_bun.py
@@ -3,7 +3,7 @@ import sys
 sys.stdin = open("input.txt", "r")
 
 
-def fish_shape_bun(n, m, k):
+def fish_shape_bun(n, m, k, arr):
     bun = 0  # 붕어빵 개수
     t = 0
     count = 0
@@ -11,20 +11,20 @@ def fish_shape_bun(n, m, k):
         t += 1
         if t % m == 0:  # m초 마다 k개 붕어빵 생성
             bun += k
-        for time in arrive_time:
-            if t == time:
-                if bun == 0:
+        for time in arr:    # 사람들 방문
+            if t == time:   # 방문한 시간에
+                if bun == 0:    # 붕어빵이 없다.
                     return "Impossible"
-                else:
-                    bun -= 1
-                    count += 1
-                    if count == n:
+                else:       # 붕어빵 있다.
+                    bun -= 1    # 붕어빵 감소
+                    count += 1  # 붕어빵 판매량
+                    if count == n:  # 목표 판매량 달성
                         return "Possible"
-    return "Possible"
+    return "Impossible"
 
 T = int(input())
 for tc in range(1, T + 1):
     N, M, K = map(int, input().split())  # N = 사람 수, M = 시간초, K = 붕어빵 개수
     arrive_time = list(map(int, input().split()))  # N명의 사람이 도착하는 시간
 
-    print(f"#{tc} {fish_shape_bun(N, M, K)}")
+    print(f"#{tc} {fish_shape_bun(N, M, K, arrive_time)}")
