1b6cc7fa7ed56ce206f2d23faca432daac863233
heewon0107 <shinheewon0107@gmail.com>
2024-08-28 08:52:03 +0900

    Subtree

--- 0827_Tree/Subtree/Subtree.py
+++ 0827_Tree/Subtree/Subtree.py
@@ -2,26 +2,29 @@ import sys
 
 sys.stdin = open("sample_input.txt", "r")
 
+
 # 탐색할 루트 찾기
 def tree(root):
-    # leaf가 없다.
-    if not left[root]:
-        return
-    # 탐색할 루트 찾음
-    if root == N:
-        subset(root)
-        return
-    tree(left[root])
-    tree(right[root])
-
-# 서브트리 탐색하기.
-def subset(x):
     global result
-    if left[x] == 0:
+    global certification    # 서브트리 자격 부여
+
+    # leaf가 없다.
+    if not root:
         return
-    result += 1
-    subset(left[x])
-    subset(right[x])
+
+    # 서브 트리면 카운팅
+    if certification == 1:
+        result += 1
+    # 서브트리 탐색할 루트 찾음
+    if root == N:
+        certification = 1   # 서브트리 카운트 자격 부여.
+        tree(left[root])
+        tree(right[root])
+        certification = 0   # 서브트리 종료 하면서 본인 노드 결과에 추가
+        result += 1
+    else:
+        tree(left[root])
+        tree(right[root])
 
 
 T = int(input())
@@ -29,9 +32,11 @@ for tc in range(1, T + 1):
     E, N = map(int, input().split())  # E = 간선의 개수, N = 루트
     arr = list(map(int, input().split()))
     adj_lst = [[] for _ in range(E + 2)]  # 인접리스트 번호
-    left = [0] * (E + 3)
-    right = [0] * (E + 3)
+    # 노드 번호는 E+1번까지 0번 인덱스는 안 씀
+    left = [0] * (E + 2)
+    right = [0] * (E + 2)
     result = 0
+    certification = 0
     for i in range(0, E * 2, 2):
         adj_lst[arr[i]].append(arr[i + 1])
 
