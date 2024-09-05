7ae8d630faa0d55c6bede119270dae4235bb2498
heewon0107 <shinheewon0107@gmail.com>
2024-09-05 17:55:28 +0900

    before2

--- 0904_분할정복/이진 탐색/Binary_search.py
+++ 0904_분할정복/이진 탐색/Binary_search.py
@@ -3,29 +3,28 @@ import sys
 sys.stdin = open("sample_input(1).txt", "r")
 
 
-def bn(s, e, n):
+def bn(s, e, n, direction):
     global result
     if s > e:
         return
     mid = s - (s - e) // 2
 
-    if B[mid] == n:
+    if A[mid] == n:
         result += 1
-
-    if B[mid] < n:
-        bn(mid + 1, e, n)
-    elif B[mid] > n:
-        bn(s, mid-1, n)
-
+        return
+    elif A[mid] < n and (direction == "" or direction != "right"):
+        bn(mid + 1, e, n, "right")
+    elif A[mid] > n and (direction == "" or direction != "left"):
+        bn(s, mid-1, n, "left")
 
 T = int(input())
 for tc in range(1, T + 1):
     N, M = map(int, input().split())
     A = list(map(int, input().split()))
     B = list(map(int, input().split()))
-    r = len(B)
     # B의 정수가 A에 들어 있는가
     result = 0
-    for i in A:
-        bn(0, r-1, i)
+    for i in B:
+        bn(0, N-1, i , "")
     print(f"#{tc} {result}")
+
