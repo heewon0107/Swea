403d79ed7508ec451227581245e00aa88f031adb
heewon0107 <shinheewon0107@gmail.com>
2024-09-06 10:27:47 +0900

    Board_Relay

--- 0906_응용_문제풀이/격자판의 숫자 이어 붙이기/Board_relay.py
+++ 0906_응용_문제풀이/격자판의 숫자 이어 붙이기/Board_relay.py
@@ -3,31 +3,24 @@ import sys
 sys.stdin = open("sample_input.txt", "r")
 
 
-def board(level, i, j):
-    global result
-    global cnt
-    if i == (4 or -1) or j == (4 or -1):
+def board(i, j, string):
+    if len(string) == 7:
+        result.add(string)
         return
 
-    result.append(arr[i][j])
-    if level == 7:
-        if result not in stack:
-            stack.append(result)
-            cnt += 1
-        result.pop()
-        return
     for di, dj in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
-        board(level + 1, i+di, j+dj)
+        ni = i + di
+        nj = j + dj
+        if 0 <= ni < 4 and 0 <= nj < 4:
+            board(ni, nj, string + arr[ni][nj])
 
 
 T = int(input())
 for tc in range(1, T + 1):
-    arr = [list(map(str, input().split())) for _ in range(4)]
-    result = []
-    cnt = 0
-    stack = []
+    arr = [input().split() for _ in range(4)]
+    result = set()
     for i in range(4):
         for j in range(4):
-            board(0, i, j)
+            board(i, j, arr[i][j])
 
-    print(f"#{tc} {cnt}")
+    print(f"#{tc} {len(result)}")
