656e3eb63a5718fd0643b451547a3fb9d179e514
heewon0107 <shinheewon0107@gmail.com>
2024-09-06 09:00:11 +0900

    Binary_search

--- 0904_분할정복/이진 탐색/Binary_search.py
+++ 0904_분할정복/이진 탐색/Binary_search.py
@@ -7,21 +7,22 @@ def bn(s, e, n, direction):
     global result
     if s > e:
         return
-    mid = s - (s - e) // 2
+    mid = s + (e - s) // 2
 
     if A[mid] == n:
         result += 1
         return
-    elif A[mid] < n and (direction == "" or direction != "right"):
-        bn(mid + 1, e, n, "right")
-    elif A[mid] > n and (direction == "" or direction != "left"):
-        bn(s, mid-1, n, "left")
+    elif A[mid] < n and (direction == "" or direction != "left"):
+        bn(mid + 1, e, n, "left")
+    elif A[mid] > n and (direction == "" or direction != "right"):
+        bn(s, mid-1, n, "right")
 
 T = int(input())
 for tc in range(1, T + 1):
     N, M = map(int, input().split())
     A = list(map(int, input().split()))
     B = list(map(int, input().split()))
+    A.sort()
     # B의 정수가 A에 들어 있는가
     result = 0
     for i in B:
