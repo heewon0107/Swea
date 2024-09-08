2f6917f58260b205ad2db071495811a260154b65
shinheewon0107 <shinheewon0107@gmail.com>
2024-09-08 13:03:06 +0900

    Mountain_Road

--- 0906_문제풀이_실습1/등산로 조성/Mountain_road.py
+++ 0906_문제풀이_실습1/등산로 조성/Mountain_road.py
@@ -3,23 +3,25 @@ import sys
 sys.stdin = open("sample_input.txt", "r")
 
 
-def mountain(i, j, cnt, visited):
+def mountain(i, j, cnt, visited, cut, h):
     global result
-    if i < 0 or i == N or j < 0 or j == N or visited[i][j]:
-        return
-    visited[i][j] = 1
+    if cnt > result:
+        result = cnt
+
     for di, dj in Delta:
-        for k in range(K + 1):
-            ni = i + di
-            nj = j + dj
-            if arr[i][j] > arr[ni][nj] - k:
-                arr[ni][nj] -= k
-                mountain(ni, nj, cnt + 1, visited)
-                arr[ni][nj] -= k
-            else:
-                if cnt > result:
-                    result = cnt
-    visited[i][j] = 0
+        ni = i + di
+        nj = j + dj
+        if 0 <= ni < N and 0 <= nj < N and (ni, nj) not in visited:
+            if arr[ni][nj] < h:
+                visited.append((ni, nj))
+                mountain(ni, nj, cnt + 1, visited, cut, arr[ni][nj])
+                visited.pop()
+            elif cut:
+                for k in range(arr[ni][nj] - K, arr[ni][nj]):
+                    if k < h:
+                        visited.append((ni, nj))
+                        mountain(ni, nj, cnt + 1, visited, 0, k)
+                        visited.pop()
 
 
 Delta = [[0, 1], [1, 0], [0, -1], [-1, 0]]
@@ -28,7 +30,16 @@ for tc in range(1, T + 1):
     N, K = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
     result = 0
+    start = []
+    high_top = 0
     for i in range(N):
         for j in range(N):
-            mountain(i, j, 0, [[0] * N for _ in range(N)])
-    print(result)
+            if high_top < arr[i][j]:
+                high_top = arr[i][j]
+                start = [(i, j)]
+            elif high_top == arr[i][j]:
+                start.append((i, j))
+
+    for r, c in start:
+        mountain(r, c, 1, [(r, c)], 1, high_top)
+    print(f"#{tc} {result}")
