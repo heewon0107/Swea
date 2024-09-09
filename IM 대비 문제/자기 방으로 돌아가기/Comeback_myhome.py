fe3ab44a99bacd082721b39ee933f73d54edca84
heewon0107 <shinheewon0107@gmail.com>
2024-09-09 13:51:09 +0900

    Comeback_myhome

---
+++ IM 대비 문제/자기 방으로 돌아가기/Comeback_myhome.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open("sample_input.txt", "r")
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    corridor = [0] * 201
+    for _ in range(N):
+        start, end = map(int, input().split())
+        if start > end:
+            start, end = end, start
+
+        start = (start + 1) // 2
+        end = (end + 1) // 2
+        for i in range(start, end+1):
+            corridor[i] += 1
+    t = max(corridor)
+    print(f"#{tc} {t}")
